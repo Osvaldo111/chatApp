83ae02b2bd97fe2871e3c521ea4c33efb19fba74
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-07-08 00:37:11 -0500

    changes in register

--- register.js
+++ register.js
@@ -23,10 +23,6 @@ const queryInsertUser = {
   values: [userName, userPassword],
 }
 
-
-//Thus us the flag
-var flag = null;
-
 // Find if the user is in the database. Otherwise 
 // insert the new user.
 pool.query(queryFindUser, (err, res) => {
@@ -44,18 +40,11 @@ pool.query(queryFindUser, (err, res) => {
         console.log(res.rows[0])
       }
     })
-  } else{
-
-    flag = res.rows[0].exists;
-    console.log("THIIs IS THE LAST ELSE");
-      
   }
 })
 
-return flag
+	}
 
 };
-
-
 
 
