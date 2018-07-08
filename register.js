eed5344789f8173ea42a09d724bb7a9c7b6a57f8
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-07-08 16:55:38 -0500

    changes

--- register.js
+++ register.js
@@ -28,7 +28,7 @@ var flag = null;
 
 // Find if the user is in the database. Otherwise 
 // insert the new user.
-pool.query(queryFindUser, (err, res, flag, callback(flag)) => {
+pool.query(queryFindUser, (err, res, flag, callback) => {
   if (err) {
     console.log(err.stack, "This is an error")
   } else if(res.rows[0].exists == false){
