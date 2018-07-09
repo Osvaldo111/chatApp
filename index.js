714ffbc1fce6247cb9c179df3e05e12150778ec1
osvaldo <car17005@byui.edu>
2018-07-09 10:58:17 -0500

    chnages

--- index.js
+++ index.js
@@ -43,11 +43,11 @@ express()
 
 
   })
-  .post('/welcome',function(req,res){
+  .get('/welcome',function(req,res){
 
 	
-	console.log(req.body.userName);
-	console.log(req.body.userPassword);
+	console.log(req.params.userName);
+	console.log(req.params.userPassword);
 
 	// Call the function to inser the user
   registerUser.registerUser(req, res, pool, function(flag){
