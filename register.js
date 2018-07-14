3d67ae6de9ce79c128de635c141eb5c186e74550
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-07-13 21:43:29 -0500

    new module

--- register.js
+++ register.js
@@ -13,7 +13,7 @@ module.exports = {
 	var userName = req.body.username;
   // The password of the user from the form
 	var userPassword = bcrypt.hashSync(req.body.password, saltRounds);
-  console.log(userName, "THIS IS THE USERNAME", userPassword, "PASS");
+  console.log(userName, "THIS IS THE USERNAME", userPassword, "PASSw");
 
 
  // Query to verify if the user exists on the database
