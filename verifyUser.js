f9e10b3a17d506e4f23664d1a41bfdfd8defa6d2
osvaldo carrillo <osvaldofabricio11@gmail.com>
2018-07-13 21:16:03 -0500

    new module

--- verifyUser.js
+++ verifyUser.js
@@ -31,7 +31,7 @@ module.exports = {
 
 			if (err) {
 		    console.log(err.stack, "This is an error")
-		  } else if(res.user_name){
+		  } else if(res.rows[0].user_name){
 
 		    // Assign false if the user doesn't exist. 
 		    //result = res.rows[0].exists;
